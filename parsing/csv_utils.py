a8291c043a9bf6abce00661f7da7f5e96187d64f
VihaanChhabria <chhabria.vihaan@gmail.com>
2025-03-02 12:06:57 -0500

    cleaned up code and seperated csv funcs

---
+++ parsing/csv_utils.py
@@ -0,0 +1,54 @@
+import csv
+import os
+
+
+def openCSV(path: str) -> list[list[str]]:
+    with open(path, "r", newline="", encoding="utf-8") as file:
+        csvReader = csv.reader(file, delimiter=";", quotechar='"')
+        formattedCSV: list[list[str]] = []
+        for row in csvReader:
+            formattedCSV.append(row)
+        return formattedCSV
+
+
+def getAllData() -> list[list[str]]:
+    fullPitData: list[list[str]] = []
+    for pitDataFileNameIndex, pitDataFileName in enumerate(
+        os.listdir("data/pit_data/")
+    ):
+        if pitDataFileNameIndex == 0:
+            fullPitData += openCSV("data/pit_data/" + pitDataFileName)
+        else:
+            fullPitData += openCSV("data/pit_data/" + pitDataFileName)[1:]
+    return fullPitData
+
+
+def getRow(csvFile: list[list[str]], rowNum: int) -> list[str]:
+    for rowIndex, row in enumerate(csvFile):
+        if rowIndex == rowNum:
+            return row
+    return []
+
+
+def getColumnByIndex(csvFile: list[list[str]], columnNum: int) -> list[str]:
+    column: list[str] = []
+    for row in csvFile:
+        for cellIndex, cell in enumerate(row):
+            if cellIndex == columnNum:
+                column.append(cell)
+    return column
+
+
+def getColumnByHeader(csvFile: list[list[str]], headerName: str) -> list[str]:
+    columnIndex: int = 0
+    for headerIndex, header in enumerate(csvFile[0]):
+        if header == headerName:
+            columnIndex = headerIndex
+
+    return getColumnByIndex(csvFile, columnIndex)
+
+
+def addColumn(currentData: list[list[str]], newColumn: list[str]):
+    for rowIndex, row in enumerate(currentData):
+        row.append(newColumn[rowIndex])
+    return currentData
--- parsing/main.py
+++ parsing/main.py
@@ -1,18 +1,10 @@
-import csv
 import requests
 from requests.auth import HTTPBasicAuth
 import os
+import csv_utils
 
 
 def loadENV(filename: str):
-    with open(filename) as f:
-        for line in f:
-            if line.strip() and not line.startswith("#"):
-                key, value = line.strip().split("=", 1)
-                os.environ[key] = value
-
-
-def envCheck():
     if not os.path.exists(".env"):
         with open(".env", "w") as f:
             for key, value in {
@@ -25,57 +17,11 @@ def envCheck():
         )
         exit()
 
-
-def openCSV(path: str) -> list[list[str]]:
-    with open(path, "r", newline="", encoding="utf-8") as file:
-        csvReader = csv.reader(file, delimiter=";", quotechar='"')
-        formattedCSV: list[list[str]] = []
-        for row in csvReader:
-            formattedCSV.append(row)
-        return formattedCSV
-
-
-def getAllData() -> list[list[str]]:
-    fullPitData: list[list[str]] = []
-    for pitDataFileNameIndex, pitDataFileName in enumerate(
-        os.listdir("data/pit_data/")
-    ):
-        if pitDataFileNameIndex == 0:
-            fullPitData += openCSV("data/pit_data/" + pitDataFileName)
-        else:
-            fullPitData += openCSV("data/pit_data/" + pitDataFileName)[1:]
-    return fullPitData
-
-
-def getRow(csvFile: list[list[str]], rowNum: int) -> list[str]:
-    for rowIndex, row in enumerate(csvFile):
-        if rowIndex == rowNum:
-            return row
-    return []
-
-
-def getColumnByIndex(csvFile: list[list[str]], columnNum: int) -> list[str]:
-    column: list[str] = []
-    for row in csvFile:
-        for cellIndex, cell in enumerate(row):
-            if cellIndex == columnNum:
-                column.append(cell)
-    return column
-
-
-def getColumnByHeader(csvFile: list[list[str]], headerName: str) -> list[str]:
-    columnIndex: int = 0
-    for headerIndex, header in enumerate(csvFile[0]):
-        if header == headerName:
-            columnIndex = headerIndex
-
-    return getColumnByIndex(csvFile, columnIndex)
-
-
-def addColumn(currentData: list[list[str]], newColumn: list[str]):
-    for rowIndex, row in enumerate(currentData):
-        row.append(newColumn[rowIndex])
-    return currentData
+    with open(filename) as f:
+        for line in f:
+            if line.strip() and not line.startswith("#"):
+                key, value = line.strip().split("=", 1)
+                os.environ[key] = value
 
 
 def getImageFromURL(url: str, teamNum: str):
@@ -90,20 +36,19 @@ def getImageFromURL(url: str, teamNum: str):
 
 
 if __name__ == "__main__":
-    getAllData()
-
-    envCheck()
     loadENV(".env")
 
     USERNAME = os.environ.get("USERNAME")
     PASSWORD = os.environ.get("PASSWORD")
 
-    data = getAllData()
+    data = csv_utils.getAllData()
 
-    picturesURLs = getColumnByHeader(data, "Take a picture of the team’s robot._URL")[
-        1:
-    ]
-    teamNums = getColumnByHeader(data, "What is the team you are pit scouting?")[1:]
+    picturesURLs = csv_utils.getColumnByHeader(
+        data, "Take a picture of the team’s robot._URL"
+    )[1:]
+    teamNums = csv_utils.getColumnByHeader(
+        data, "What is the team you are pit scouting?"
+    )[1:]
 
     for pictureURL, teamNum in zip(picturesURLs, teamNums):
         getImageFromURL(pictureURL, teamNum)
